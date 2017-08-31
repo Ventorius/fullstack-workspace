b5d9f117c91820767ef46bf62fca989f2f4738b0
Sebastian Basznianin <avaven@gmail.com>
2017-09-01 00:09:00 +0200

    some changes

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const express = require('express')
 const mongoose = require('mongoose')
 const passport = require('passport')
 const cookieSession = require('cookie-session')
+const bodyParser = require('body-parser')
 
 const keys = require('./config/keys')
 require('./models/User')
 require('./services/passport')
 
-
 mongoose.connect(keys.mongoURI)
 
 const app = express()
+
+app.use(bodyParser.json())
 
 app.use(
   cookieSession({
@@ -22,9 +24,17 @@ app.use(
 app.use(passport.initialize())
 app.use(passport.session())
 
-
-
 require('./routes/authRoutes')(app)
+require('./routes/billingRoutes')(app)
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'))
+
+  const path  = require('path')
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
